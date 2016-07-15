84ad5ac37cef4430c391e38bd3238f3280ca133a
Vasyl <kiriljuk.vasja@gmail.com>
2016-07-15 23:52:42 +0300

    facts

--- scripts/base.js
+++ scripts/base.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $("a").click(function () { 
       var elementClick = $(this).attr("href");
       var destination = $(elementClick).offset().top;
-      $('html,body').animate( { scrollTop: destination }, 700 );
+      $('html,body').animate( { scrollTop: destination }, 900 );
       return false;
     });
   });
@@ -14,7 +14,7 @@ $(document).ready(function() {
 
 	$(window).on('scroll', function() {
         var scrollNow = $(window).scrollTop();
-		if(scrollNow > scrollTop && scrollNow > 5){
+		if(scrollNow > scrollTop && scrollNow > 20){
 		    $('.header').addClass('setout');
 		} else {
 			$('.header').removeClass('setout');
