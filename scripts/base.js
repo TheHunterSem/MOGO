96c6dca95e1ad86d008c9af4817c74276081e580
Vasyl <kiriljuk.vasja@gmail.com>
2016-07-16 17:21:56 +0300

    service

--- scripts/base.js
+++ scripts/base.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $("a").click(function () { 
       var elementClick = $(this).attr("href");
       var destination = $(elementClick).offset().top;
-      $('html,body').animate( { scrollTop: destination }, 900 );
+      $('html,body').animate( { scrollTop: destination }, 1000 );
       return false;
     });
   });
