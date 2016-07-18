03a2c6ba59a5dc27e71a5bd5971da945559b637d
Vasyl <kiriljuk.vasja@gmail.com>
2016-07-18 04:53:12 +0300

    footer

--- scripts/base.js
+++ scripts/base.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     $("a").click(function () { 
       var elementClick = $(this).attr("href");
       var destination = $(elementClick).offset().top;
-      $('html,body').animate( { scrollTop: destination }, 1000 );
+      $('html,body').animate( { scrollTop: destination }, 1200 );
       return false;
     });
   });
